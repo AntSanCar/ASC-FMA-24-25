98967f7c7b36c14edc5aaaf8a150d416fe8fed40
FRANCISCO MORENO ALCARAZ <framoralc@alu.edu.gva.es>
2024-12-13 20:32:01 +0100

    He añadido un textbox un label llamado "Texto" en negrita y he cambiado el tamaño de la ventana

--- EjercicioTelegrama/EjercicioTelegrama/Form1.Designer.cs
+++ EjercicioTelegrama/EjercicioTelegrama/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblInfo1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 104);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblInfo1
+            // 
+            this.lblInfo1.AutoSize = true;
+            this.lblInfo1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblInfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo1.Location = new System.Drawing.Point(12, 20);
+            this.lblInfo1.Name = "lblInfo1";
+            this.lblInfo1.Size = new System.Drawing.Size(39, 13);
+            this.lblInfo1.TabIndex = 1;
+            this.lblInfo1.Text = "Texto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(491, 243);
+            this.Controls.Add(this.lblInfo1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblInfo1;
     }
 }
 
